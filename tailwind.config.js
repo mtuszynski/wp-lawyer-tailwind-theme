27522b8f7a0ccef341bea7a843af9483cca049c5
mtuszynski <tuszynski.mir@gmail.com>
2024-10-03 13:58:26 +0200

    feat: Add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,51 @@
-const theme = require('./theme.json');
+const theme = require("./theme.json");
 const tailpress = require("@jeffreyvr/tailwindcss-tailpress");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './*.php',
-        './**/*.php',
-        './resources/css/*.css',
-        './resources/js/*.js',
-        './safelist.txt'
-    ],
-    theme: {
-        container: {
-            padding: {
-                DEFAULT: '1rem',
-                sm: '2rem',
-                lg: '0rem'
-            },
-        },
-        extend: {
-            colors: tailpress.colorMapper(tailpress.theme('settings.color.palette', theme)),
-            fontSize: tailpress.fontSizeMapper(tailpress.theme('settings.typography.fontSizes', theme))
-        },
-        screens: {
-            'xs': '480px',
-            'sm': '600px',
-            'md': '782px',
-            'lg': tailpress.theme('settings.layout.contentSize', theme),
-            'xl': tailpress.theme('settings.layout.wideSize', theme),
-            '2xl': '1440px'
-        }
+  content: [
+    "./*.php",
+    "./**/*.php",
+    "./resources/css/*.css",
+    "./resources/js/*.js",
+    "./safelist.txt",
+  ],
+  theme: {
+    container: {
+      screens: {
+        xs: "100%",
+        sm: "100%",
+        md: "100%",
+        lg: "100%",
+        xl: "100%",
+        "2xl": "1460px",
+      },
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1rem",
+        lg: "0rem",
+      },
     },
-    plugins: [
-        tailpress.tailwind
-    ]
+    extend: {
+      colors: tailpress.colorMapper(
+        tailpress.theme("settings.color.palette", theme)
+      ),
+      fontSize: tailpress.fontSizeMapper(
+        tailpress.theme("settings.typography.fontSizes", theme)
+      ),
+      fontFamily: {
+        body: ["Playfair Display", "serif"],
+        header: ["Playfair Display SC", "serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "600px",
+      md: "782px",
+      lg: "992px",
+      xl: "1280px",
+      "2xl": "1460px",
+    },
+  },
+  plugins: [tailpress.tailwind],
 };
